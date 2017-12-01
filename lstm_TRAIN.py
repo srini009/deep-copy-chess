0e61b3ae9f9768163108af9591523d967237cd34
Srinivasan Ramesh <sramesh@talapas-ln1.uoregon.edu>
2017-12-01 00:07:03 -0800

    Add some changes to predict multiple outputs

--- lstm_TRAIN.py
+++ lstm_TRAIN.py
@@ -4,6 +4,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
+from keras.layers import TimeDistributed
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 # load ascii text and covert to lowercase
@@ -19,33 +20,38 @@ n_vocab = len(chars)
 print ("Total Characters: ", n_chars)
 print ("Total Vocab: ", n_vocab)
 # prepare the dataset of input to output pairs encoded as integers
-seq_length = 100
+seq_length = 20
 dataX = []
 dataY = []
-for i in range(0, n_chars - seq_length, 1):
+for i in range(0, n_chars - 10*seq_length, 1):
 	seq_in = raw_text[i:i + seq_length]
-	seq_out = raw_text[i + seq_length]
+	seq_out = raw_text[i + seq_length: i + 2*seq_length]
 	dataX.append([char_to_int[char] for char in seq_in])
-	dataY.append(char_to_int[seq_out])
+	dataY.append([char_to_int[char] for char in seq_out])
+
 n_patterns = len(dataX)
 print ("Total Patterns: ", n_patterns)
+
 # reshape X to be [samples, time steps, features]
+n_output_patterns = len(dataY)
 X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
 # normalize
 X = X / float(n_vocab)
 # one hot encode the output variable
-y = np_utils.to_categorical(dataY)
+#y = np_utils.to_categorical(dataY)
+Y = numpy.reshape(dataY, (n_output_patterns, seq_length))
+Y = Y / float(n_vocab)
 # define the LSTM model
 model = Sequential()
 model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
 model.add(Dropout(0.2))
 model.add(LSTM(256))
 model.add(Dropout(0.2))
-model.add(Dense(y.shape[1], activation='softmax'))
+model.add(Dense(seq_length, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 # define the checkpoint
-filepath="weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
+filepath="weights.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 # fit the model
-model.fit(X, y, epochs=1, batch_size=128, callbacks=callbacks_list)
+model.fit(X, Y, epochs=10, batch_size=128, callbacks=callbacks_list)
