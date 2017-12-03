46ee37eab7df6a49e4aa93160b01e0275daae432
Manish Mathai <mmathai@uoregon.edu>
2017-12-03 13:52:07 -0800

    Multiprocess pgn files

--- convert_pgn.py
+++ convert_pgn.py
@@ -1,9 +1,13 @@
 import sys
+import multiprocessing
 import time
 
 import chess.pgn
 
 from chess import SQUARES_180, BB_SQUARES, BB_FILE_H, H1
+
+
+BATCH_SIZE = 1000
 
 
 def board_to_string(board):
@@ -40,21 +44,17 @@ def load_games(pgn_file_name):
                 return
 
 
-def main():
-    if len(sys.argv) < 3:
-        print("Usage: {} <path_to_pgn> <path_to_output_file>".format(sys.argv[0]))
-        sys.exit(1)
-
-    output_dir_path = sys.argv[2]
+def convert_pgn(args):
+    prefix, pgn_file_name, output_dir_path = args
     batch_count = 0
     total_count = 0
-    start_time = time.time()
-    total_start_time = time.time()
     file_count = 1
-    output_file_name = "{}/{}.txt".format(output_dir_path, file_count)
-    output_file = open(output_file_name, "w")
-    for game in load_games(sys.argv[1]):
+    output_file_name = None
+    for game in load_games(pgn_file_name):
         try:
+            if not output_file_name:
+                output_file_name = "{}/{}-{}.txt".format(output_dir_path, prefix, file_count)
+                output_file = open(output_file_name, "w")
             convertor = GameConvertor()
             game.accept(convertor)
             if not convertor.valid:
@@ -62,23 +62,33 @@ def main():
             batch_count += 1
             output_file.write(convertor.to_string())
             output_file.write("\n")
-            if batch_count == 10000:
+            if batch_count == BATCH_SIZE:
                 output_file.close()
+                output_file_name = None
                 file_count += 1
-                output_file_name = "{}/{}.txt".format(output_dir_path, file_count)
-                output_file = open(output_file_name, "w")
-                elapsed_time = (time.time() - start_time)
-                print("Converted {} games in {:.2f} seconds".format(batch_count, elapsed_time))
                 total_count += batch_count
                 batch_count = 0
-                start_time = time.time()
         except Exception as e:
-            print("Exception occured  \"{}\". Continuing")
+            print("Exception occured  \"{}\". Continuing".format(e))
     output_file.close()
-    if batch_count != 10000:
+    if batch_count != 0:
         total_count += batch_count
-    elapsed_time = (time.time() - total_start_time)
-    print("Totally converted games {} in {:.2f} seconds".format(total_count, elapsed_time))
+    return total_count
+
+
+def main():
+    if len(sys.argv) < 4:
+        print("Usage: {} <path_to_pgn1> ...... <path_to_output_dir>".format(sys.argv[0]))
+        sys.exit(1)
+
+    start_time = time.time()
+    convert_args = [(i, sys.argv[i], sys.argv[-1]) for i in range(1, len(sys.argv) - 1)]
+    pool = multiprocessing.Pool(8)
+    counts = pool.map(convert_pgn, convert_args)
+    elapsed_time = time.time() - start_time
+    total_count = sum(counts)
+    rate = total_count / elapsed_time
+    print("Converted {} games in {:.2f} seconds. Average rate = {:.3f} games/second".format(total_count, elapsed_time, rate))
 
 if __name__ == '__main__':
     main()
