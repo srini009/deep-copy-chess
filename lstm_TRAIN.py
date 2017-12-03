637270d5ae7f855ffeb2f5b6eb03d989569b73a4
Srinivasan Ramesh <sramesh@talapas-ln1.uoregon.edu>
2017-12-02 20:35:21 -0800

    Trying many-to-many

--- lstm_TRAIN.py
+++ lstm_TRAIN.py
@@ -36,50 +36,35 @@ print ("Total Patterns: ", n_patterns)
 
 # reshape X to be [samples, time steps, features]
 n_output_patterns = len(dataY)
-X = numpy.reshape(dataX, (n_patterns, 1, seq_length))
+X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
 # normalize
 X = X / float(n_vocab)
-
-Y = numpy.reshape(dataY, (n_output_patterns, seq_length))
+Y = numpy.reshape(dataY, (n_patterns, seq_length, 1))
 Y = Y / float(n_vocab)
-# define the LSTM model
+# create LSTM
 model = Sequential()
-model.add(LSTM(seq_length, input_shape=(X.shape[1], X.shape[2])))
-model.add(Dense(seq_length))
-#model.compile(loss='categorical_crossentropy', optimizer='adam')
+model.add(LSTM(seq_length, input_shape=(seq_length, 1), return_sequences=True))
+model.add(TimeDistributed(Dense(1)))
 model.compile(loss='mean_squared_error', optimizer='adam')
+
 print(model.summary())
 
 # define the checkpoint
 filepath="weights.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
-# fit the model
-model.fit(X, Y, epochs=10, batch_size=128, callbacks=callbacks_list)
+
+model.fit(X, Y, epochs=5, batch_size=64, verbose=2)
+
 
 #Generate stuff
-model.load_weights(filepath)
-# pick a random seed
 start = numpy.random.randint(0, len(dataX)-1)
 pattern = dataX[start]
 print ("Seed:")
 print ("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
-# generate characters
-x = numpy.reshape(pattern, (1, 1, len(pattern)))
+x = numpy.reshape(pattern, (1, len(pattern), 1))
 x = x / float(n_vocab)
 prediction = model.predict(x, verbose=0)
-prediction = prediction[0]
-prediction_string = prediction*float(n_vocab)
-print ("Prediction is: ", prediction_string)
 
-#for i in range(20):
-#        x = numpy.reshape(pattern, (1, len(pattern), 1))
-#        x = x / float(n_vocab)
-#        prediction = model.predict(x, verbose=0)
-#        index = numpy.argmax(prediction)
-#        result = int_to_char[index]
-#        seq_in = [int_to_char[value] for value in pattern]
-#        sys.stdout.write(result)
-#        pattern.append(index)
-#        pattern = pattern[1:len(pattern)]
-#print ("\nDone.")
+print ("Input: ", x)
+print ("Output: ", prediction)
